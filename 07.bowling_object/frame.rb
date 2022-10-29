5af48a90001478af8d20ce654ce72de63f083b86
kekosh <kenta0540@gmail.com>
2022-10-29 14:26:27 +0900

    frameクラス実装

--- 07.bowling_object/frame.rb
+++ 07.bowling_object/frame.rb
@@ -1,4 +1,36 @@
 # frozen_string_literal: true
 
+require 'minitest/autorun'
+require_relative './shot'
+
 class Frame
+  def initialize(first_shot, second_shot, third_shot = 0)
+    @first_shot = first_shot
+    @second_shot = second_shot
+    @third_shot = third_shot
+  end
+
+  def score
+    Shot.new(@first_shot).score + Shot.new(@second_shot).score + Shot.new(@third_shot).score
+  end
+end
+
+class FrameTest < Minitest::Test
+  def setup
+    @shot_point = '4'
+    @shot_half = '5'
+    @shot_strike = 'X'
+    @shot_gatter = '0'
+  end
+
+  def test_return_frame_score
+    # nomal frame
+    assert_equal 9, Frame.new(@shot_point, @shot_half).score
+    # gatter frame
+    assert_equal 4, Frame.new(@shot_point, @shot_gatter).score
+    # strke frame
+    assert_equal 30, Frame.new(@shot_strike, @shot_strike, @shot_strike).score
+    # spare frame
+    assert_equal 14, Frame.new(@shot_half, @shot_half, @shot_point).score
+  end
 end
