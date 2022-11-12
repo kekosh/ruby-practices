49ee321b9a341dea74dc1063d6425d186a2ba5f4
kekosh <kenta0540@gmail.com>
2022-11-12 14:55:24 +0900

    メイン処理ファイルからテスト実行用コードを削除 game.rbのテストコードを分離

--- 07.bowling_object/frame.rb
+++ 07.bowling_object/frame.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
-require 'minitest/autorun'
 require_relative './shot'
 
 class Frame
@@ -12,25 +11,5 @@ class Frame
 
   def score
     @first_shot + @second_shot + @third_shot
-  end
-end
-
-class FrameTest < Minitest::Test
-  def setup
-    @shot_point = 4
-    @shot_half = 5
-    @shot_strike = 10
-    @shot_gatter = 0
-  end
-
-  def test_return_frame_score
-    # nomal frame
-    assert_equal 9, Frame.new(@shot_point, @shot_half).score
-    # gatter frame
-    assert_equal 4, Frame.new(@shot_point, @shot_gatter).score
-    # strke frame
-    assert_equal 30, Frame.new(@shot_strike, @shot_strike, @shot_strike).score
-    # spare frame
-    assert_equal 14, Frame.new(@shot_half, @shot_half, @shot_point).score
   end
 end
--- 07.bowling_object/shot.rb
+++ 07.bowling_object/shot.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require 'minitest/autorun'
-
 class Shot
   def initialize(shot)
     @shot = shot
@@ -9,19 +7,5 @@ class Shot
 
   def score
     @shot == 'X' ? 10 : @shot.to_i
-  end
-end
-
-class ShotTest < Minitest::Test
-  def setup
-    @shot_point = '5'
-    @shot_gatter = '0'
-    @shot_strike = 'X'
-  end
-
-  def test_return_score
-    assert_equal 5, Shot.new(@shot_point).score
-    assert_equal 0, Shot.new(@shot_gatter).score
-    assert_equal 10, Shot.new(@shot_strike).score
   end
 end
