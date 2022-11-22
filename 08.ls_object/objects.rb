914bfb079c5ca60f56f2b07c62c1877b74cd990a
kekosh <kenta0540@gmail.com>
2022-11-22 14:32:38 +0900

    objectモジュールにファイル一覧を取得する処理を実装

--- 08.ls_object/objects.rb
+++ 08.ls_object/objects.rb
@@ -4,4 +4,17 @@ class Objects
   def initialize(options)
     @options = options
   end
+
+  def object_list
+    objects = if @options[:all]
+                Dir.glob('*', File::FNM_DOTMATCH)
+              else
+                Dir.glob('*')
+              end
+
+    # Ruby3.1から親ディレクトリを表す".."がDir.globで取得されなくなったため対応
+    objects.push('..') if @options[:all]
+
+    @options[:reverse] ? objects.sort.reverse : objects.sort
+  end
 end
