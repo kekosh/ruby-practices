0adabde932b75d446d2ac4962ec07403e35807bf
kekosh <kenta0540@gmail.com>
2022-11-22 14:37:25 +0900

    テストフォルダ内のテストファイルを管理対象に追加

---
+++ 08.ls_object/test/objects_test.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+require 'minitest/autorun'
+require_relative '../objects'
+
+class ObjectsTest < Minitest::Test
+  def setup
+    @no_options = { all: false, list: false, reverse: false }
+    @option_all_only = { all: true, list: false, reverse: false }
+    @option_reverse_only = { all: false, list: false, reverse: true }
+    @option_all_reverse = { all: true, list: false, reverse: true }
+
+    @no_option_result = ['information.rb', 'ls.rb', 'objects.rb', 'show.rb', 'test']
+    @option_all_result = ['.', '..', '.gitkeep', 'information.rb', 'ls.rb', 'objects.rb', 'show.rb', 'test']
+  end
+
+  # -aオプション
+  # ディレクトリ内の一般的なファイル、ディレクトリのリストを取得する
+  def test_should_return_normal_objects_list
+    assert_equal @no_option_result, Objects.new(@no_options).object_list
+  end
+
+  # ディレクトリ内の全オブジェクトを取得する
+  def test_should_return_all_bjects_list
+    assert_equal @option_all_result, Objects.new(@option_all_only).object_list
+  end
+
+  # -rオプション
+  # 名前の降順でディレクトリ内の オブジェクトリストを取得
+  def test_should_return_object_list_sorted_decending_by_name
+    assert_equal @no_option_result.reverse, Objects.new(@option_reverse_only).object_list
+  end
+
+  # ディレクトリ内全オブジェクトのリストを降順で取得
+  def test_should_return_all_objects_list_sorted_decending_by_name
+    assert_equal @option_all_result.reverse, Objects.new(@option_all_reverse).object_list
+  end
+
+  # [Note]-lオプションによる詳細情報の取得はファイル取得とは別枠なので不要？
+  # -lオプション
+  # 詳細情報とともにオブジェクトとの一覧を出力する。
+  # def test_should_return_objects_list_with_detail_infomation
+  # end
+end
