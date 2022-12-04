964480172f8a3469bdf95173e2f784224055aa96
kekosh <kenta0540@gmail.com>
2022-12-04 12:15:50 +0900

    自作lsコマンドクラスが動作するよう各処理を統合

--- 08.ls_object/information.rb
+++ 08.ls_object/information.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'etc'
+
 class Information
   FILE_TYPE = {
     'file' => '-',
--- 08.ls_object/ls.rb
+++ 08.ls_object/ls.rb
@@ -2,6 +2,9 @@
 # frozen_string_literal: true
 
 require 'optparse'
+require_relative './objects'
+require_relative './show'
+require_relative './information'
 
 opt = OptionParser.new
 options = { all: false, list: false, reverse: false }
@@ -11,3 +14,15 @@ opt.on('-l') { |v| options[:list] = v }
 opt.on('-r') { |v| options[:reverse] = v }
 
 opt.parse! { ARGV }
+
+object_list = if options[:list]
+                Information.new(Objects.new(options).object_list).return_detail_info_and_permissions
+              else
+                Objects.new(options).object_list
+              end
+
+if options[:list]
+  puts Show.new(object_list).show_object_detail
+else
+  puts Show.new(object_list).show_object_list
+end
