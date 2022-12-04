bed8378b4f7e151581469358830beba15738f668
kekosh <kenta0540@gmail.com>
2022-12-05 00:50:12 +0900

    [リファクタリング] ls.rbから各クラスのインスタンスを呼び出していたのを Showクラスのインスタンスのみ呼び出すよう修正

--- 08.ls_object/ls.rb
+++ 08.ls_object/ls.rb
@@ -2,9 +2,7 @@
 # frozen_string_literal: true
 
 require 'optparse'
-require_relative './objects'
 require_relative './show'
-require_relative './information'
 
 opt = OptionParser.new
 options = { all: false, list: false, reverse: false }
@@ -15,10 +13,4 @@ opt.on('-r') { |v| options[:reverse] = v }
 
 opt.parse! { ARGV }
 
-object_list = Objects.new(options).object_list
-
-if options[:list]
-  puts Show.new(Information.new(object_list).return_detail_info_and_permissions).show_object_detail
-else
-  puts Show.new(object_list).show_object_list
-end
+puts Show.new(options).object_list
