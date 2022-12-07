d0958242c432b3c615f9bd5624817b933bdd8b43
kekosh <kenta0540@gmail.com>
2022-12-07 23:56:43 +0900

    [リファクタリング] ls.rbにおいてオプションの値で条件分岐して別々のShowクラスのインスタンスメソッドを 呼び出していたのを、メソッドを共通化してインスタンスメソッド内でオプション別の 戻り値を返すように修正

--- 08.ls_object/show.rb
+++ 08.ls_object/show.rb
@@ -1,27 +1,35 @@
 # frozen_string_literal: true
 
 require_relative './information'
+require_relative './objects'
 
 class Show
   ITEMS_ON_THE_LINE = 3
 
-  def initialize(objects)
-    @objects = objects
+  def initialize(options)
+    @options = options
   end
 
-  def show_object_list
-    output_rows = (@objects.size / ITEMS_ON_THE_LINE.to_f).ceil
+  def object_list
+    @options[:list] ? return_object_detail_info_list : return_object_name_list
+  end
+
+  private
+
+  def return_object_name_list
+    objects = Objects.new(@options).object_list
+    output_rows = (objects.size / ITEMS_ON_THE_LINE.to_f).ceil
     array_by_row = Array.new(output_rows) { [] }
     index = 0
 
-    @objects.each do |object|
+    objects.each do |object|
       array_by_row[index] << object
       index += 1
       index = 0 if index == output_rows
     end
 
     output_string = ''
-    longest_word_length = @objects.max_by(&:length).length
+    longest_word_length = objects.max_by(&:length).length
     array_by_row.each do |row|
       output_string += row.map { |item| item.ljust(longest_word_length) }.join("\t\t")
       output_string += "\n"
@@ -29,11 +37,12 @@ class Show
     output_string.strip
   end
 
-  def show_object_detail
-    total_block_size, object_list = @objects
+  def return_object_detail_info_list
+    informations = Information.new(@options).return_detail_info_and_permissions
+    total_block_size, object_info_list = informations
     output_string = "total #{total_block_size}\n"
-    object_list.each do |object|
-      output_string += "#{object}\n"
+    object_info_list.each do |object_info|
+      output_string += "#{object_info}\n"
     end
     output_string.strip
   end
--- 08.ls_object/test/show_test.rb
+++ 08.ls_object/test/show_test.rb
@@ -5,29 +5,81 @@ require_relative '../show'
 
 class ShowTest < Minitest::Test
   def setup
-    @data_in_a_row = ['.', 'for_test', 'test1.rb', 'test2.rb', 'test3.txt']
-    @data_list_option = [24, [
-      'drwxr-xr-x  6 kekosh  staff  192 11 27 16:57 .',
-      'drwxr-xr-x  3 kekosh  staff   96 11 23 20:38 for_test',
-      '-rw-r--r--  1 kekosh  staff   58 11 24 19:11 test1.rb',
-      '-rw-r--r--  1 kekosh  staff   73 11 24 19:13 test2.rb',
-      '-rw-r--r--  1 kekosh  staff  211 11 27 16:57 test3.txt'
-    ]]
+    # ------ テストデータ
+    # オプションなし
+    @no_options = { all: false, list: false, reverse: false }
+    # allオプションのみ
+    @option_all_only = { all: true, list: false, reverse: false }
+    # listオプションのみ
+    @option_list_only = { all: false, list: true, reverse: false }
+    # reverseオプションのみ
+    @option_reverse_only = { all: false, list: false, reverse: true }
+    # allとlistオプション
+    @option_all_and_list = { all: true, list: true, reverse: false }
+    # allとreverseオプション
+    @option_all_and_reverse = { all: true, list: false, reverse: true }
+    # 全てのオプション
+    @all_options_turned_on = { all: true, list: true, reverse: true }
 
-    @data_in_a_row_result = ".        \t\ttest1.rb \t\ttest3.txt\nfor_test \t\ttest2.rb"
-    @file_name_and_details_in_a_row_result =
-      "total 24\ndrwxr-xr-x  6 kekosh  staff  192 11 27 16:57 .\n" \
+    # ------ 想定結果
+    # オプションなし
+    @no_options_result = "for_test \t\ttest2.rb \ntest1.rb \t\ttest3.txt"
+    # allオプション
+    @option_all_only_result = ".        \t\ttest1.rb \t\ttest3.txt\nfor_test \t\ttest2.rb"
+    # listオプション
+    @option_list_only_result =
+      "total 24\n" \
       "drwxr-xr-x  3 kekosh  staff   96 11 23 20:38 for_test\n" \
       "-rw-r--r--  1 kekosh  staff   58 11 24 19:11 test1.rb\n" \
       "-rw-r--r--  1 kekosh  staff   73 11 24 19:13 test2.rb\n" \
       '-rw-r--r--  1 kekosh  staff  211 11 27 16:57 test3.txt'
+    # reverseオプション
+    @option_reverse_only_result = "test3.txt\t\ttest1.rb \ntest2.rb \t\tfor_test"
+    # all + listオプション
+    @option_all_and_list_result =
+      "total 24\n" \
+      "drwxr-xr-x  6 kekosh  staff  192 11 27 16:57 .\n" \
+      "drwxr-xr-x  3 kekosh  staff   96 11 23 20:38 for_test\n" \
+      "-rw-r--r--  1 kekosh  staff   58 11 24 19:11 test1.rb\n" \
+      "-rw-r--r--  1 kekosh  staff   73 11 24 19:13 test2.rb\n" \
+      '-rw-r--r--  1 kekosh  staff  211 11 27 16:57 test3.txt'
+    # all + reverseオプション
+    @option_all_and_reverse_result = "test3.txt\t\ttest1.rb \t\t.        \ntest2.rb \t\tfor_test"
+    # すべてのオプション
+    @all_options_are_turned_on_result =
+      "total 24\n" \
+      "-rw-r--r--  1 kekosh  staff  211 11 27 16:57 test3.txt\n" \
+      "-rw-r--r--  1 kekosh  staff   73 11 24 19:13 test2.rb\n" \
+      "-rw-r--r--  1 kekosh  staff   58 11 24 19:11 test1.rb\n" \
+      "drwxr-xr-x  3 kekosh  staff   96 11 23 20:38 for_test\n" \
+      'drwxr-xr-x  6 kekosh  staff  192 11 27 16:57 .'
   end
 
-  def test_should_return_filename_up_to_3_filename_in_a_row
-    assert_equal @data_in_a_row_result, Show.new(@data_in_a_row).show_object_list
+  def test_should_return_filename_list
+    assert_equal @no_options_result, Show.new(@no_options).object_list
   end
 
-  def test_should_return_filename_and_details
-    assert_equal @file_name_and_details_in_a_row_result, Show.new(@data_list_option).show_object_detail
+  def test_should_return_all_object_list
+    assert_equal @option_all_only_result, Show.new(@option_all_only).object_list
+  end
+
+  def test_should_return_filename_and_details_list
+    assert_equal @option_list_only_result, Show.new(@option_list_only).object_list
+  end
+
+  def test_should_return_filename_reverse_sort_list
+    assert_equal @option_reverse_only_result, Show.new(@option_reverse_only).object_list
+  end
+
+  def test_should_return_all_objects_name_and_details_list
+    assert_equal @option_all_and_list_result, Show.new(@option_all_and_list).object_list
+  end
+
+  def test_should_return_all_objects_name_sort_reverse_list
+    assert_equal @option_all_and_reverse_result, Show.new(@option_all_and_reverse).object_list
+  end
+
+  def test_should_return_list_all_options_are_on
+    assert_equal @all_options_are_turned_on_result, Show.new(@all_options_turned_on).object_list
   end
 end
