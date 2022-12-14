cbb7c8499bfda468e6f3e9c0c96a7094cba4db07
kekosh <kenta0540@gmail.com>
2022-12-15 02:06:28 +0900

    [レビュー指摘事項修正] objectsのテストコード修正（テストデータをローカル変数に変更）

--- 08.ls_object/test/objects_test.rb
+++ 08.ls_object/test/objects_test.rb
@@ -4,35 +4,34 @@ require 'minitest/autorun'
 require_relative '../objects'
 
 class ObjectsTest < Minitest::Test
-  def setup
-    @no_options = { all: false, list: false, reverse: false }
-    @option_all_only = { all: true, list: false, reverse: false }
-    @option_reverse_only = { all: false, list: false, reverse: true }
-    @option_all_reverse = { all: true, list: false, reverse: true }
-
-    @no_option_result = ['for_test', 'test1.rb', 'test2.rb', 'test3.txt']
-    @option_all_result = ['.', 'for_test', 'test1.rb', 'test2.rb', 'test3.txt']
-  end
 
   # -aオプション
   # ディレクトリ内の一般的なファイル、ディレクトリのリストを取得する
   def test_should_return_normal_objects_list
-    assert_equal @no_option_result, Objects.new(@no_options).object_list
+    no_options = { all: false, list: false, reverse: false }
+    no_option_result = ['for_test', 'test1.rb', 'test2.rb', 'test3.txt']
+    assert_equal no_option_result, Objects.new(no_options).object_list
   end
 
   # ディレクトリ内の全オブジェクトを取得する
   def test_should_return_all_objects_list
-    assert_equal @option_all_result, Objects.new(@option_all_only).object_list
+    option_all_only = { all: true, list: false, reverse: false }
+    option_all_result = ['.', 'for_test', 'test1.rb', 'test2.rb', 'test3.txt']
+    assert_equal option_all_result, Objects.new(option_all_only).object_list
   end
 
   # -rオプション
   # 名前の降順でディレクトリ内の オブジェクトリストを取得
   def test_should_return_object_list_sorted_decending_by_name
-    assert_equal @no_option_result.reverse, Objects.new(@option_reverse_only).object_list
+    option_reverse_only = { all: false, list: false, reverse: true }
+    no_option_result = ['for_test', 'test1.rb', 'test2.rb', 'test3.txt']
+    assert_equal no_option_result.reverse, Objects.new(option_reverse_only).object_list
   end
 
   # ディレクトリ内全オブジェクトのリストを降順で取得
   def test_should_return_all_objects_list_sorted_decending_by_name
-    assert_equal @option_all_result.reverse, Objects.new(@option_all_reverse).object_list
+    option_all_reverse = { all: true, list: false, reverse: true }
+    option_all_result = ['.', 'for_test', 'test1.rb', 'test2.rb', 'test3.txt']
+    assert_equal option_all_result.reverse, Objects.new(option_all_reverse).object_list
   end
 end
